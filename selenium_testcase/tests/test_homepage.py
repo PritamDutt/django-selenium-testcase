dbed9476a804470ff44c5e5fb830826872479ea9
Brian Schott <brian.schott@nimbisservices.com>
2017-10-15 11:56:06 -0400

    Add test_homepage to unit tests.
    This commit tests the SeleniumLiveTestCase by loading the '/'
    page and checking for the content in the default template defined
    by the TEST_TEMPLATE setting.

---
+++ selenium_testcase/tests/test_homepage.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from ..testcases import SeleniumLiveTestCase
+
+
+class HomepageTestCase(SeleniumLiveTestCase):
+
+    test_templates = [(r'', 'test.html')]
+
+    def test_homepage(self):
+        """
+        Test a simple home page.
+        """
+        self.get_page("/")
+        self.should_see("This is a test.")
--- test_project/settings.py
+++ test_project/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'test_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'test_project', 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'test_project', 'templates'),
+            os.path.join(BASE_DIR, 'selenium_testcase', 'tests', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -85,20 +87,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+AUTH_PASSWORD_VALIDATORS = []
 
 
 # Internationalization
