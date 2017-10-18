b5d812504924af2e2781f4be63a6191e5c47879d
Brian Schott <brian.schott@nimbisservices.com>
2017-10-18 16:43:08 -0400

    Support multiple templates in TEST_TEMPLATES setting.
    Unit tests need to be able to test redirects and other features
    involving multiple web pages.  This commit changes the singleton
    TEST_TEMPLATE setting to TEST_TEMPLATES, which is a list of
    path, template tuples.

--- test_project/urls.py
+++ test_project/urls.py
@@ -18,10 +18,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-TEST_TEMPLATE = getattr(settings, 'TEST_TEMPLATE', 'test.html')
-
 urlpatterns = [
     url('^accounts/', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name=TEST_TEMPLATE)),
 ]
+
+TEST_TEMPLATES = getattr(
+    settings, 'TEST_TEMPLATES', [(r'^$', 'test.html')])
+
+for path, template in TEST_TEMPLATES:
+    urlpatterns.append(url(path, TemplateView.as_view(template_name=template)))
