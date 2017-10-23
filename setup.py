d007a0cc9388281e9ee73beeeaa850a0af4cb200
Brian Schott <brian.schott@nimbisservices.com>
2017-10-22 21:14:44 -0400

    Add .travis.yml, MANIFEST.in, and setup.py.
    Required for initial build and continuous testing on travis.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+# setup the project
+setup(
+    name="django-selenium-testcase",
+    version="1.0.0rc0",
+    author="Nimbis Services, Inc.",
+    author_email="info@nimbisservices.com",
+    description="Selenium helper methods for Django live server testing.",
+    license="BSD",
+    packages=find_packages(exclude=["test_project", ]),
+    install_requires=[
+        'Django',
+        'selenium'
+    ],
+    zip_safe=False,
+    include_package_data=True,
+)
