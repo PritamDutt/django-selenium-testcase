90bc34a6242dcf2ed9eb80025e8199c812a972dc
Brian Schott <brian.schott@nimbisservices.com>
2017-10-24 11:32:26 -0400

    Update setup/CHANGES to version 1.0.0rc4.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 # setup the project
 setup(
     name="django-selenium-testcase",
-    version="1.0.0rc0",
+    version="1.0.0rc4",
     author="Nimbis Services, Inc.",
     author_email="info@nimbisservices.com",
     description="Selenium helper methods for Django live server testing.",
