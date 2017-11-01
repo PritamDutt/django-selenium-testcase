1f37fe0757439b6b7252fc7e4cca7a9488af00e6
Brian Schott <brian.schott@nimbisservices.com>
2017-11-01 11:58:10 -0400

    Add find_text to FindTestMixin.
    This commit adds a find_test method to FindTestMixin because the
    find_element path isn't reliable for page content checks.  The
    exception handling is consolidated into a private method for both
    find_element and find_exception.

--- selenium_testcase/testcases/find.py
+++ selenium_testcase/testcases/find.py
@@ -22,6 +22,36 @@ class FindTestMixin:
     # dump png image uri on missing element
     png_on_missing = PNG_ON_MISSING
 
+    def _raise_no_such_element(self, message, *args, **kwargs):
+        """ Raise missing exception with extra features. """
+
+        if kwargs.get('text', self.text_on_missing):
+            message += "---\n" + self.get_visible_text() + "\n---\n"
+
+        # dump image data link to exception message
+        if kwargs.get('png', self.png_on_missing):
+            message += "---\n" + self.get_image_uri() + "\n---\n"
+
+        # exit to pdb if flag is set
+        if kwargs.get('pdb', self.pdb_on_missing):
+            import pdb
+            pdb.set_trace()
+
+        # raise the exception if we get this far
+        raise NoSuchElementException(message)
+
+    def find_text(self, text, *args, **kwargs):
+        """ Return true if text is found on page. """
+
+        visible_text = self.get_visible_text()
+
+        if text in visible_text:
+            return True
+
+        # if we get this far, raise exception
+        message = "Unable to find {} on page:\n".format(text)
+        self._raise_no_such_element(message, *args, **kwargs)
+
     def find_element(self, search_list, *args, **kwargs):
         """ Return a single element by optional index. """
         elements = self.find_elements(search_list, *args, **kwargs)
@@ -61,18 +91,5 @@ class FindTestMixin:
                 if elements[index].is_displayed():
                     return elements
 
-        # dump raw page text to exception message
-        if kwargs.get('text', self.text_on_missing):
-            message += "---\n" + self.get_visible_text() + "\n---\n"
-
-        # dump image data link to exception message
-        if kwargs.get('png', self.png_on_missing):
-            message += "---\n" + self.get_image_uri() + "\n---\n"
-
-        # exit to pdb if flag is set
-        if kwargs.get('pdb', self.pdb_on_missing):
-            import pdb
-            pdb.set_trace()
-
         # raise the exception if we get this far
-        raise NoSuchElementException(message)
+        self._raise_no_such_element(message, *args, **kwargs)
