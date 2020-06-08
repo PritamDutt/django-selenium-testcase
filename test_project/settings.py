83577f9a6a31e85974d2eb69e04306f784f0a403
Brian Schott <brian.schott@nimbisservices.com>
2020-06-08 15:23:49 -0400

    Restore postgres database configuration in test_projct settings.py.
    This reverts commit 37303cb09dd57f199b797f239d4d3bf72bdfbaf2.

--- test_project/settings.py
+++ test_project/settings.py
@@ -85,9 +85,13 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'ci',
+       'USER': 'postgres',
+       'PASSWORD': 'postgres',
+       'HOST': 'postgres',
+       'PORT': '5432',
+    },
 }
 
 # Password validation
